b73755fbdd23d608855bb158177ca0f82aa0ad1c
sjpurusothaman <purushothaman.sj@janaanalysis.com>
2023-01-20 15:31:59 +0530

    Route Issue

--- src/app/views/academics/ClassroomSetup.jsx
+++ src/app/views/academics/ClassroomSetup.jsx
@@ -303,7 +303,7 @@ const ClassroomSetup = (props) => {
                     <br />
                     <br />
                   <span style={{ display: "flex", color: "#1DA1F2", flexDirection: "row", justifyContent: "flex-end", alignItems: "center" }}>
-                      <Link  to={{ pathname: '/academic/subject-management', query: { classes:"12-" + sect } }} ><Typography variant='p' style={{cursor:"pointer"}}>View Class </Typography></Link>
+                      <Link to='/academic/subject-management' state={{ classes:"12-" + sect }} ><Typography variant='p' style={{cursor:"pointer"}}>View Class </Typography></Link>
                       <ArrowForwardIosIcon fontSize='14px' />
                     </span>
                   </ClassCard>
